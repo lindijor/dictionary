8c4eb9791445f8da084dee07a70fc347df9773aa
lindijor <jorgensenlindis@gmail.com>
2024-01-20 16:54:44 +0100

    Added html for header and search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return "hello";
+  return (
+    <div className="header">
+      <h1>Dictionary</h1>
+      <h2>What do you want to learn more about?</h2>
+      <form>
+        <input
+          type="search"
+          placeholder="Type a word or a phrase..."
+          className="searchBar"
+        />
+        <input type="submit" value="Search" className="searchButton" />
+      </form>
+      <p>
+        <i>e.g. flower, sunset, chair, giraffe...</i>
+      </p>
+    </div>
+  );
 }
